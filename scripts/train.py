c6adc7226f13078caab8739415695089e3b59913
hmxf <gouych13@icloud.com>
2023-11-03 05:37:50 +0800

    A fully operational version of "save_model" function.

--- scripts/train.py
+++ scripts/train.py
@@ -13,4 +13,14 @@ model = StackedDenseNet(train_generator)
 
 model.fit(batch_size=16, n_workers=8)
 
-keras.saving.save_model(model, filepath='saved_model.keras', overwrite=True)
+# There are two methods can use for saving trained model. 
+# Althouht model.save() is an alias for keras_core.saving.save_model(), 
+# but the former works fine but the latter doesn't, weird.
+
+# Just choose the format you like. 
+# This statement can be output normally for models in both formats.
+# model.train_model.save('data/saved_model.keras')
+model.train_model.save('data/saved_model.h5')
+
+# This cannot work. Maybe someone can try dig in and find why.
+# keras.saving.save_model(model, filepath='saved_model.keras', overwrite=True)
