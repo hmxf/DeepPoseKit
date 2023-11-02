e93031ff9be8a9b5f716adcc0b666c6e6b892d7d
hmxf <gouych13@icloud.com>
2023-11-03 02:33:50 +0800

    Replace self-designed load function with Keras_core official API

--- deepposekit/models/__init__.py
+++ deepposekit/models/__init__.py
@@ -21,4 +21,4 @@ from deepposekit.models.LEAP import LEAP
 from deepposekit.models.DeepLabCut import DeepLabCut
 
 #from deepposekit.models.saving import save_model
-from deepposekit.models.loading import load_model
+#from deepposekit.models.loading import load_model
--- scripts/predict.py
+++ scripts/predict.py
@@ -5,7 +5,7 @@ import keras_core as keras
 from deepposekit.models import load_model
 from deepposekit.io import VideoReader
 
-model = load_model('data/saved_model.h5')
+model = keras.saving.load_model(filepath='data/saved_model.keras', custom_objects=None, compile=True, safe_mode=True)
 reader = VideoReader('data/video.avi')
 
 predictions = model.predict(reader)
